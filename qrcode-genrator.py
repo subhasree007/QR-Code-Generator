61d79b328e9bee5897062ac84722b604cf554a30
subhasree <subhasreepsahoo@gmail.com>
2021-06-01 23:56:50 +0530

    Create qrcode-genrator.py
    QR Code Generate Link

---
+++ qrcode-genrator.py
@@ -0,0 +1,23 @@
+import qrcode
+from qrcode.main import QRCode
+
+
+qr = qrcode.QRCode(
+
+    version= 1,
+    box_size= 15,
+    border = 15
+)
+
+link = "http://subhasreesahoo.com/#"
+
+qr.add_data(link)
+qr.make(fit=True)
+
+img = qr.make_image(fill = 'black',back_color = 'white')
+
+img.save("weblink.png")
+
+
+
+
